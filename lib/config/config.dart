e0cf5a504f3eb56622d048edf6969d578ae44a91
Yannick <lanfyp@gmail.com>
2023-08-29 23:06:01 +0200

    config

---
+++ lib/config/config.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Config {
+  static final colors = _Color();
+  static const apiKey = 'DEB67FCF7320B43B63500F664CDBC648';
+}
+
+class _Color {
+  final primaryColor = const Color(0xFF000000);
+  final primaryTextColor = const Color(0xFFFFFFFF);
+  final accentTextColor = const Color(0xFFbf8700);
+}
